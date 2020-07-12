a89951324bde636301e5a09bbc6afaae2febac5c
Giel <gieljnssns@me.com>
2020-07-12 12:14:54 +0200

    Update kostal sensor to use the config flow

--- custom_components/kostal/__init__.py
+++ custom_components/kostal/__init__.py
@@ -1,1 +1,54 @@
 """The kostal component."""
+import voluptuous as vol
+
+
+from homeassistant.config_entries import SOURCE_IMPORT, ConfigEntry
+from homeassistant.const import (
+    CONF_NAME,
+    CONF_HOST,
+    CONF_USERNAME,
+    CONF_PASSWORD,
+    CONF_MONITORED_CONDITIONS,
+)
+import homeassistant.helpers.config_validation as cv
+from homeassistant.helpers.typing import HomeAssistantType
+
+from .const import DEFAULT_NAME, DOMAIN, SENSOR_TYPES
+
+CONFIG_SCHEMA = vol.Schema(
+    {
+        DOMAIN: vol.Schema(
+            {
+                vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
+                vol.Required(CONF_USERNAME): cv.string,
+                vol.Required(CONF_PASSWORD): cv.string,
+                vol.Required(CONF_HOST): cv.string,
+                vol.Required(CONF_MONITORED_CONDITIONS): vol.All(
+                    cv.ensure_list, [vol.In(list(SENSOR_TYPES))]
+                ),
+            }
+        )
+    },
+    extra=vol.ALLOW_EXTRA,
+)
+
+
+async def async_setup(hass, config):
+    """Platform setup, do nothing."""
+    if DOMAIN not in config:
+        return True
+
+    hass.async_create_task(
+        hass.config_entries.flow.async_init(
+            DOMAIN, context={"source": SOURCE_IMPORT}, data=dict(config[DOMAIN])
+        )
+    )
+    return True
+
+
+async def async_setup_entry(hass: HomeAssistantType, entry: ConfigEntry):
+    """Load the saved entities."""
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    )
+    return True
--- custom_components/kostal/sensor.py
+++ custom_components/kostal/sensor.py
@@ -1,70 +1,40 @@
-"""Support for Kostal Piko inverters."""
-from datetime import timedelta
+"""The Kostal piko integration."""
+
+
 import logging
 
 from kostalpyko.kostalpyko import Piko
 
-import voluptuous as vol
 
-from homeassistant.components.sensor import PLATFORM_SCHEMA
 from homeassistant.const import (
-    CONF_USERNAME, CONF_PASSWORD, CONF_HOST, CONF_MONITORED_CONDITIONS, CONF_NAME)
+    CONF_USERNAME,
+    CONF_PASSWORD,
+    CONF_HOST,
+    CONF_MONITORED_CONDITIONS,
+)
+
 from homeassistant.helpers.entity import Entity
 from homeassistant.util import Throttle
-import homeassistant.helpers.config_validation as cv
 
-MIN_TIME_BETWEEN_UPDATES = timedelta(seconds=30)
+
+from .const import SENSOR_TYPES, MIN_TIME_BETWEEN_UPDATES, DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
-DEFAULT_NAME = "kostal_piko"
 
-SENSOR_TYPES = {
-    'solar_generator_power': ['Solar generator power', 'W', 'mdi:solar-power'],
-    'consumption_phase_1': ['Consumption phase 1', 'W', 'mdi:power-socket-eu'],
-    'consumption_phase_2': ['Consumption phase 2', 'W', 'mdi:power-socket-eu'],
-    'consumption_phase_3': ['Consumption phase 3', 'W', 'mdi:power-socket-eu'],
-    'current_power': ['Current power', 'W', 'mdi:solar-power'],
-    'total_energy': ['Total energy', 'kWh', 'mdi:solar-power'],
-    'daily_energy': ['Daily energy', 'kWh', 'mdi:solar-power'],
-    'string1_voltage': ['String 1 voltage', 'V', 'mdi:current-ac'],
-    'string1_current': ['String 1 current', 'A', 'mdi:flash'],
-    'string2_voltage': ['String 2 voltage', 'V', 'mdi:current-ac'],
-    'string2_current': ['String 2 current', 'A', 'mdi:flash'],
-    'string3_voltage': ['String 3 voltage', 'V', 'mdi:current-ac'],
-    'string3_current': ['String 3 current', 'A', 'mdi:flash'],
-    'l1_voltage': ['L1 voltage', 'V', 'mdi:current-ac'],
-    'l1_power': ['L1 power', 'W', 'mdi:power-plug'],
-    'l2_voltage': ['L2 voltage', 'V', 'mdi:current-ac'],
-    'l2_power': ['L2 power', 'W', 'mdi:power-plug'],
-    'l3_voltage': ['L3 voltage', 'V', 'mdi:current-ac'],
-    'l3_power': ['L3 power', 'W', 'mdi:power-plug'],
-    'status': ['Status', None, 'mdi:solar-power'],
-}
+async def async_setup_entry(hass, entry, async_add_entities):
+    """Add an Kostal piko entry."""
+    # Add the needed sensors to hass
+    piko = Piko(
+        entry.data[CONF_HOST], entry.data[CONF_USERNAME], entry.data[CONF_PASSWORD]
+    )
+    data = PikoData(piko, hass)
 
+    entities = []
 
-PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Required(CONF_USERNAME): cv.string,
-    vol.Required(CONF_PASSWORD): cv.string,
-    vol.Required(CONF_HOST): cv.string,
-    vol.Required(CONF_MONITORED_CONDITIONS):
-        vol.All(cv.ensure_list, [vol.In(list(SENSOR_TYPES))]),
-    vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
-})
-
-
-def setup_platform(hass, config, add_entities, discovery_info=None):
-    """Set up Piko inverter."""
-    piko = Piko(config[CONF_HOST],
-                config[CONF_USERNAME],
-                config[CONF_PASSWORD])
-
-    dev = []
-    data = PikoData(piko)
-    for sensor in config[CONF_MONITORED_CONDITIONS]:
-        dev.append(PikoInverter(data, sensor, config[CONF_NAME]))
-
-    add_entities(dev)
+    for sensor in entry.data[CONF_MONITORED_CONDITIONS]:
+        entities.append(PikoInverter(data, sensor, entry.title))
+    async_add_entities(entities)
 
 
 class PikoInverter(Entity):
@@ -79,12 +49,14 @@ class PikoInverter(Entity):
         self._state = None
         self._unit_of_measurement = SENSOR_TYPES[self.type][1]
         self._icon = SENSOR_TYPES[self.type][2]
+        self.serial_number = None
+        self.model = None
         self.update()
 
     @property
     def name(self):
         """Return the name of the sensor."""
-        return '{} {}'.format(self._name, self._sensor)
+        return "{} {}".format(self._name, self._sensor)
 
     @property
     def state(self):
@@ -101,97 +73,114 @@ class PikoInverter(Entity):
         """Return icon."""
         return self._icon
 
+    @property
+    def unique_id(self):
+        """Return unique id based on device serial and variable."""
+        return "{} {}".format(self.serial_number, self._sensor)
+
+    @property
+    def device_info(self):
+        """Return information about the device."""
+        return {
+            "identifiers": {(DOMAIN, self.serial_number)},
+            "name": self._name,
+            "manufacturer": "Kostal",
+            "model": self.model,
+        }
+
     def update(self):
         """Update data."""
         self.piko.update()
         data = self.piko.data
         ba_data = self.piko.ba_data
+        self.serial_number = self.piko.info[0]
+        self.model = self.piko.info[1]
         if ba_data is not None:
-            if self.type == 'solar_generator_power':
+            if self.type == "solar_generator_power":
                 if len(ba_data) > 1:
                     self._state = ba_data[5]
                 else:
                     return "No BA sensor installed"
-            elif self.type == 'consumption_phase_1':
+            elif self.type == "consumption_phase_1":
                 if len(ba_data) > 1:
                     self._state = ba_data[8]
                 else:
                     return "No BA sensor installed"
-            elif self.type == 'consumption_phase_2':
+            elif self.type == "consumption_phase_2":
                 if len(ba_data) > 1:
                     self._state = ba_data[9]
                 else:
                     return "No BA sensor installed"
-            elif self.type == 'consumption_phase_3':
+            elif self.type == "consumption_phase_3":
                 if len(ba_data) > 1:
                     self._state = ba_data[10]
                 else:
                     return "No BA sensor installed"
         if data is not None:
-            if self.type == 'current_power':
+            if self.type == "current_power":
                 if len(data) > 1:
                     self._state = data[0]
                 else:
                     return None
-            elif self.type == 'total_energy':
+            elif self.type == "total_energy":
                 if len(data) > 1:
                     self._state = data[1]
                 else:
                     return None
-            elif self.type == 'daily_energy':
+            elif self.type == "daily_energy":
                 if len(data) > 1:
                     self._state = data[2]
                 else:
                     return None
-            elif self.type == 'string1_voltage':
+            elif self.type == "string1_voltage":
                 if len(data) > 1:
                     self._state = data[3]
                 else:
                     return None
-            elif self.type == 'string1_current':
+            elif self.type == "string1_current":
                 if len(data) > 1:
                     self._state = data[5]
-            elif self.type == 'string2_voltage':
+            elif self.type == "string2_voltage":
                 if len(data) > 1:
                     self._state = data[7]
                 else:
                     return None
-            elif self.type == 'string2_current':
+            elif self.type == "string2_current":
                 if len(data) > 1:
                     self._state = data[9]
                 else:
                     return None
-            elif self.type == 'string3_voltage':
+            elif self.type == "string3_voltage":
                 if len(data) > 1:
                     self._state = data[0]
                 else:
                     return None
-            elif self.type == 'string3_current':
+            elif self.type == "string3_current":
                 if len(data) > 1:
                     self._state = data[0]
                 else:
                     return None
-            elif self.type == 'l1_voltage':
+            elif self.type == "l1_voltage":
                 if len(data) > 1:
                     self._state = data[4]
                 else:
                     return None
-            elif self.type == 'l1_power':
+            elif self.type == "l1_power":
                 if len(data) > 1:
                     self._state = data[6]
                 else:
                     return None
-            elif self.type == 'l2_voltage':
+            elif self.type == "l2_voltage":
                 if len(data) > 1:
                     self._state = data[8]
                 else:
                     return None
-            elif self.type == 'l2_power':
+            elif self.type == "l2_power":
                 if len(data) > 1:
                     self._state = data[10]
                 else:
                     return None
-            elif self.type == 'l3_voltage':
+            elif self.type == "l3_voltage":
                 if len(data) > 1:
                     if len(data) < 15:
                         # 2 Strings
@@ -201,7 +190,7 @@ class PikoInverter(Entity):
                         self._state = data[12]
                 else:
                     return None
-            elif self.type == 'l3_power':
+            elif self.type == "l3_power":
                 if len(data) > 1:
                     if len(data) < 15:
                         # 2 Strings
@@ -211,7 +200,7 @@ class PikoInverter(Entity):
                         self._state = data[14]
                 else:
                     return None
-            elif self.type == 'status':
+            elif self.type == "status":
                 if len(data) > 1:
                     if len(data) < 15:
                         # 2 Strings
@@ -219,22 +208,34 @@ class PikoInverter(Entity):
                     else:
                         # 3 Strings
                         self._state = data[15]
+
                 else:
                     return None
 
 
 class PikoData(Entity):
     """Representation of a Piko inverter."""
-    def __init__(self, piko):
+
+    def __init__(self, piko, hass):
         """Initialize the data object."""
+        self.piko = piko
+        self.hass = hass
         self.data = []
         self.ba_data = []
-        self.piko = piko
-    
+        self.info = None
+        self.info_update()
+
     @Throttle(MIN_TIME_BETWEEN_UPDATES)
     def update(self):
         """Update inverter data."""
+        # pylint: disable=protected-access
         self.data = self.piko._get_raw_content()
         self.ba_data = self.piko._get_content_of_own_consumption()
         _LOGGER.debug(self.data)
         _LOGGER.debug(self.ba_data)
+
+    def info_update(self):
+        """Update inverter info."""
+        # pylint: disable=protected-access
+        self.info = self.piko._get_info()
+        _LOGGER.debug(self.info)
